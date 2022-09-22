c061d57aa0a3041f52883551f040fb45706206ec
Talwinder Singh <talwindersingh@smartdatainc.net>
2022-09-22 17:34:28 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
+import { HOME_VIP_LAUNCH_MOBILE } from './asset';
 import './App.css';
 
 function App() {
+  const videoRef = useRef();
+  const sourceRef = useRef();
+  const [videoUrl, setVideoUrl] = useState(HOME_VIP_LAUNCH_MOBILE);
+
+  const handlePlay = () => {
+    videoRef.current.muted = true;
+    videoRef.current.pause();
+    sourceRef.current.setAttribute('src', videoUrl);
+    sourceRef.current.setAttribute('type', 'video/mp4');
+    videoRef.current.load();
+    videoRef.current.play();
+    videoRef.current.muted = false;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <video 
+        ref={videoRef} 
+        muted 
+        loop={true} 
+        playsInline
+      >
+        <source ref={sourceRef}></source>
+      </video>
+      <div className="App">
+
+        <div className='input-wrapper'>
+          <input 
+            value={videoUrl}
+            onChange={(e) => { setVideoUrl(e.target.value) }}
+          />
+          <button onClick={handlePlay}>Play</button>
+        </div>
+
+        <div>
+          <p>Current video url:</p>
+          <p>{videoUrl}</p>
+        </div>
+      </div>
+    </>
   );
 }
 
